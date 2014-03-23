d71cf252f8068d6738c8f2742d8f1e75880535dc
Dan Sanderson <contact@dansanderson.com>
2014-03-22 19:36:05 -0700

    Updated file list in playground. Remove unused placeholder Python code.

--- main.py
+++
@@ -1,7 +0,0 @@
-import webapp2
-
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('[TODO: run Java sample]')
-
-app = webapp2.WSGIApplication([('/', MainHandler)], debug=True)
