71e92507cdc9525eddbc5a1b4617514677073af6
Dan Sanderson <contact@dansanderson.com>
2014-03-24 14:42:36 -0700

    Change Java sample to print something.

--- src/main/java/myapp/DemoServlet.java
+++ src/main/java/myapp/DemoServlet.java
@@ -8,6 +8,6 @@ public class DemoServlet extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
         resp.setContentType("text/plain");
-        resp.getWriter().println("{ \"xyz\": 123 }");
+        resp.getWriter().println("{ \"name\": \"World\" }");
     }
 }
